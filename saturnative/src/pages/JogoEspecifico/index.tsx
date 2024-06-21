0fe36180aac372ca171c88fdc810e031146e6e8c
Khezac <khezac1@gmail.com>
2024-06-21 10:44:59 -0300

    feat: FlatList da página de jogos

---
+++ saturnative/src/pages/JogoEspecifico/index.tsx
@@ -0,0 +1,61 @@
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+const lista = [
+    {
+        id: 1,
+        name: "nome do jogo",
+        img: require('../../../assets/splash.png')
+    },
+    {
+        id: 2,
+        name: "nome do jogo",
+        img: require('../../../assets/splash.png')
+    },
+    {
+        id: 3,
+        name: "nome do jogo",
+        img: require('../../../assets/splash.png')
+    },
+    {
+        id: 4,
+        name: "nome do jogo",
+        img: require('../../../assets/splash.png')
+    },
+    {
+        id: 5,
+        name: "nome do jogo",
+        img: require('../../../assets/splash.png')
+    },
+]
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={lista}
+                keyExtractor={item => item.id.toString()}
+                ItemSeparatorComponent={() => <View style={{height: 20}}/>}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.jogoCard} >
+                            <Image style={styles.jogoImg} source={item.img} />
+                            <View style={styles.jogoInfo}>
+                                <Text style={styles.jogoName}>
+                                    {item.name}
+                                </Text>
+                                <TouchableOpacity style={styles.jogoBtn}>
+                                    <Text style={styles.jogoBtnText}>
+                                        Detalhes
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    )
+                }}
+            />
+        </View>
+    );
+};
+
+export default Home;
